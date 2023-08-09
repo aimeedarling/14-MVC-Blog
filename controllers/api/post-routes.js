3eb823e6dc3340b43c0a460f6c61271f9f1e0eae
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-08-09 16:20:10 -0700

    current changes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth')
+const Post = require('../../models')
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            body: req.body.body,
+            user_id: req.session.user_id,
+            date_created: req.body.date_created,
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
---
+++ public/JS/post.js
@@ -0,0 +1,1 @@
+// const post_id = document.querySelector('#hidden-id')
